2b309cf86688c9b14bd8a712b6beb24159f7f922
michaelkmwaura <mikemwaura91@gmail.com>
2023-12-12 17:53:15 +0000

    commit

--- main.js
+++ main.js
@@ -36,4 +36,21 @@ links.forEach((e)=>{
     e.addEventListener("click",()=>{
         opacity.style.display = "none"
     })
-})
+});
+const elements = document.querySelectorAll('.fade-in');
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .4
+}
+const callbacks = (entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting){
+      entry.target.classList.add('active');
+    }
+  });
+}
+let observer = new IntersectionObserver(callbacks, options);
+elements.forEach(element => {
+  observer.observe(element);
+});
